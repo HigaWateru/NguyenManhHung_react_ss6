9207361f55ccaac5db31d93e1a91cb5139278cae
Nguyễn Mạnh Hùng <fc.canhcut1@gmail.com>
2025-08-11 22:22:57 +0700

    second commit

--- src/ss6_3.ts
+++ src/ss6_3.ts
@@ -23,3 +23,16 @@ class Tiger extends Animal2 {
 const tiger = new Tiger()
 tiger.speak()
 console.log(`Tốc độ chạy: ${tiger.speedRun()}`)
+
+/**
+ * Method: 
+ * Là hàm được định nghĩa có thân hàm và có thể chạy ngay khi gọi
+ * Có thể chứa logic, câu lệnh, vòng lặp, ...
+ * Không bắt buộc lớp con phải Override
+ * 
+ * Abstract Method:
+ * Được khai báo bằng từ khoá abstract
+ * Không có thân hàm, chỉ có khai báo
+ * Chỉ xuất hiện bên trong abstract class hoặc interface
+ * Bắt buộc lớp con phải Override và viết code thực thi
+ */
